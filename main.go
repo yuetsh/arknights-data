456d9052f5c63ab725d0eba468ba6c7329a29adf
yuetsh <517252939@qq.com>
2019-06-16 22:50:04 +0800

    feat: add more info

--- main.go
+++ main.go
@@ -14,19 +14,34 @@ import (
 
 // 干员信息
 type Agent struct {
-	Name        string `json:"name"`
-	EnglishName string `json:"english_name"`
-	Slogan      string `json:"slogan"`
-	Class       string `json:"class"`
-	Star        string `json:"star"`
-	Group       string `json:"group"`
-	Tag         string `json:"tag"`
-	Character   string `json:"character"`
-	Record      string `json:"record"`
-	Avatar      string `json:"avatar"`
-	Image1      string `json:"image_1"`
-	Image2      string `json:"image_2"`
-	Link        string `json:"link"`
+	Name        string  `json:"name"`
+	EnglishName string  `json:"english_name"`
+	Slogan      string  `json:"slogan"`
+	Class       string  `json:"class"`
+	Star        string  `json:"star"`
+	Group       string  `json:"group"`
+	Profile     Profile `json:"profile"`
+	Tag         string  `json:"tag"`
+	Character   string  `json:"character"`
+	Record      string  `json:"record"`
+	Image       Image   `json:"image"`
+	Link        string  `json:"link"`
+}
+
+type Profile struct {
+	Position string `json:"position"`
+	Mastery  string `json:"mastery"`
+	XP       string `json:"xp"`
+	From     string `json:"from"`
+	Birthday string `json:"birthday"`
+	Race     string `json:"race"`
+	Height   string `json:"height"`
+	Status   string `json:"status"`
+}
+
+type Image struct {
+	Image1 string `json:"image_1"`
+	Image2 string `json:"image_2"`
 }
 
 var AllAgents []Agent
@@ -55,41 +70,12 @@ func fetchAgents() {
 					link := s.Find("tr").First().Find("a")
 					title, _ := link.Attr("title")
 					href, _ := link.Attr("href")
-					img, _ := link.Find("img").Attr("src")
-					img = strings.ReplaceAll(img, "/dr/150__", "")
-					agent := Agent{Name: title, Link: "http://wiki.joyme.com" + href, Avatar: img}
+					agent := Agent{
+						Name: title,
+						Link: "http://wiki.joyme.com" + href,
+					}
 					r.Geziyor.Get(agent.Link, func(r *geziyor.Response) {
-						// 基本信息
-						slogan := r.DocHTML.Find("#mw-content-text > div:nth-child(8) > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > big > big > b")
-						englishName := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(3) > div > div:nth-child(1) > table > tbody > tr:nth-child(1) > td")
-						table := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(3) > div > div:nth-child(2) > table > tbody")
-						class := table.Find("tr:nth-child(1) > td:nth-child(2)")
-						group := table.Find("tr:nth-child(2) > td:nth-child(2)")
-						star := table.Find("tr:nth-child(1) > td:nth-child(4)")
-						character := table.Find("tr:nth-child(5) > td")
-						tag := table.Find("tr:nth-child(6) > td")
-
-						agent.Slogan = slogan.Text()
-						agent.EnglishName = strings.TrimSpace(englishName.Text())
-						agent.Class = strings.TrimSpace(class.Text())
-						agent.Group = strings.TrimSpace(group.Text())
-						agent.Star = strings.TrimSpace(star.Text())
-						agent.Character = strings.TrimSpace(character.Text())
-						agent.Tag = strings.TrimSpace(tag.Text())
-
-						// 履历信息
-						record := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(4) > div > div:nth-child(2) > table > tbody > tr:nth-child(2) > td")
-						agent.Record = strings.TrimSpace(record.Text())
-
-						// 图片信息
-						img1 := r.DocHTML.Find("#con_1 > div > div > a > img")
-						if img, exists := img1.Attr("src"); exists {
-							agent.Image1 = strings.ReplaceAll(img, "/dr/1120__", "")
-						}
-						img2 := r.DocHTML.Find("#con_2 > div > div > a > img")
-						if img, exists := img2.Attr("src"); exists {
-							agent.Image2 = strings.ReplaceAll(img, "/dr/1120__", "")
-						}
+						getAgentDetail(&agent, r)
 					})
 					AllAgents = append(AllAgents, agent)
 				})
@@ -117,13 +103,68 @@ func downloadAgentsImage() {
 	for _, agent := range AllAgents {
 		wg.Add(1)
 		go func(agent Agent) {
-			DownloadImage(agent.Name, "avatar", agent.Avatar)
-			DownloadImage(agent.Name, "image_1", agent.Image1)
-			DownloadImage(agent.Name, "image_2", agent.Image2)
+			DownloadImage(agent.Name, "image_1", agent.Image.Image1)
+			DownloadImage(agent.Name, "image_2", agent.Image.Image2)
 			wg.Done()
 			log.Println("Downloaded", agent.Name)
 		}(agent)
 	}
 	wg.Wait()
 	log.Println("Finished")
+}
+
+func getAgentDetail(agent *Agent, r *geziyor.Response) {
+	// Slogan 和 英文名
+	slogan := r.DocHTML.Find("#mw-content-text > div:nth-child(8) > table > tbody > tr > td:nth-child(2) > div:nth-child(1) > big > big > b")
+	englishName := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(3) > div > div:nth-child(1) > table > tbody > tr:nth-child(1) > td")
+	agent.Slogan = slogan.Text()
+	agent.EnglishName = strings.TrimSpace(englishName.Text())
+
+	// 基本信息
+	basicTable := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(3) > div > div:nth-child(2) > table > tbody")
+	class := basicTable.Find("tr:nth-child(1) > td:nth-child(2)")
+	group := basicTable.Find("tr:nth-child(2) > td:nth-child(2)")
+	star := basicTable.Find("tr:nth-child(1) > td:nth-child(4)")
+	character := basicTable.Find("tr:nth-child(5) > td")
+	tag := basicTable.Find("tr:nth-child(6) > td")
+
+	agent.Class = strings.TrimSpace(class.Text())
+	agent.Group = strings.TrimSpace(group.Text())
+	agent.Star = strings.TrimSpace(star.Text())
+	agent.Character = strings.TrimSpace(character.Text())
+	agent.Tag = strings.TrimSpace(tag.Text())
+
+	// 履历信息
+	record := r.DocHTML.Find("#mw-content-text > div.mwiki_hide > div > div:nth-child(4) > div > div:nth-child(2) > table > tbody > tr:nth-child(2) > td")
+	agent.Record = strings.TrimSpace(record.Text())
+
+	// 档案信息
+	profileTable := r.DocHTML.Find("#mw-content-text > div.tj-big > div.tj-bg-right > div.tj-bgs.wiki_hide > table.wikitable > tbody")
+	position := profileTable.Find("tr:nth-child(1) > td")
+	mastery := profileTable.Find("tr:nth-child(2) > td")
+	xp := profileTable.Find("tr:nth-child(3) > td")
+	from := profileTable.Find("tr:nth-child(4) > td")
+	birthday := profileTable.Find("tr:nth-child(5) > td")
+	race := profileTable.Find("tr:nth-child(6) > td")
+	height := profileTable.Find("tr:nth-child(7) > td")
+	status := profileTable.Find("tr:nth-child(8) > td")
+
+	agent.Profile.Position = strings.TrimSpace(position.Text())
+	agent.Profile.Mastery = strings.TrimSpace(mastery.Text())
+	agent.Profile.XP = strings.TrimSpace(xp.Text())
+	agent.Profile.From = strings.TrimSpace(from.Text())
+	agent.Profile.Birthday = strings.TrimSpace(birthday.Text())
+	agent.Profile.Race = strings.TrimSpace(race.Text())
+	agent.Profile.Height = strings.TrimSpace(height.Text())
+	agent.Profile.Status = strings.TrimSpace(status.Text())
+
+	// 图片信息
+	img1 := r.DocHTML.Find("#con_1 > div > div > a > img")
+	if img, exists := img1.Attr("src"); exists {
+		agent.Image.Image1 = strings.ReplaceAll(img, "/dr/1120__", "")
+	}
+	img2 := r.DocHTML.Find("#con_2 > div > div > a > img")
+	if img, exists := img2.Attr("src"); exists {
+		agent.Image.Image2 = strings.ReplaceAll(img, "/dr/1120__", "")
+	}
 }
